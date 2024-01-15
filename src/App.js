287b82857165cde05d51a985b1e986ab227b49b2
AbuAbdullohm <mmprogramming3@gmail.com>
2024-01-15 16:28:03 +0500

    table created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import BasicModal from "./modal";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { users: [] };
+
+  componentDidMount() {
+    this.getUsersList();
+  }
+
+  getUsersList = () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => this.setState({ users: data }))
+      .catch((err) => console.error(err));
+  };
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div className="App">
+        <h1>My Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>№</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          {users.map((item, key) => {
+            return (
+              <tbody>
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.username}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.email}</td>
+                  <td>
+                    {/* <button onClick={BasicModal}>Edit</button> */}
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
